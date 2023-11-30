5517523730fc8b0d431d7d057bba8a5157baea24
Esrah Boulton <esrah.boulton@gmail.com>
2023-11-30 05:55:37 -0800

    accordion block heading

--- js/component/accordion/accordion-heading.js
+++ js/component/accordion/accordion-heading.js
@@ -4,7 +4,16 @@
     $('.accordion__heading').each(function(){
       if($(this).parents('.content-row').find('.content-row__title').length <= 0) {
         $(this).replaceWith(function(){
-          return this.outerHTML.replace("<h3", "<h2").replace("</h3", "</h2")
+          return this.outerHTML.replace("<h3", "<h2").replace("</h3", "</h2");
+        });
+      }
+    });
+  }
+  if($('.accordion-block__heading').length) {
+    $('.accordion-block__heading').each(function(){
+      if($(this).parents('.block').find('> h2').length > 0) {
+        $(this).replaceWith(function(){
+          return this.outerHTML.replace("<h2", "<h3").replace("</h2", "</h3");
         });
       }
     });
